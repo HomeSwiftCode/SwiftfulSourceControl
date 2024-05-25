1d303b2483032d483c19cbcc7a13217612e550f7
Oleg Chernobelsky <chy.oleg.code@gmail.com>
2024-05-25 16:17:01 +0300

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Oleg Chernobelsky on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
